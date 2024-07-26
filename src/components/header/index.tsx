8b0a9aa1ec488e42f961167d7d49e282895201bd
robertoalyssom <robert_alys@hotmail.com>
2024-07-26 18:11:51 -0300

    add bootstrap

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,7 @@ function Header() {
             <a href="#"><img src="../../../assets/images/logo.png" alt="" className="header-logo" /></a>
           </h1>
         <div>
-          <h2 className='header-title'>AI Fiction hub</h2>
+          <h2 className='header-title fs-2'>AI Fiction hub</h2>
           <p>Tales from Artificial Intelligence</p>
         </div>
     </header>
