c9299a6c91d47541cb7ed6e839ec892d38c70302
robertoalyssom <robert_alys@hotmail.com>
2024-08-04 20:45:00 -0300

    add MainCard component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,29 @@
+import "./styles.css";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+
+function MainCard() {
+  return (
+    <>
+      <Col sm={6} lg={4} className="alig-items-center justify-content-center">
+        <div className="card-wrapper">
+          <Card className="text-center card-main">
+            <Card.Img
+              variant="top"
+              src="../../../assets/images/dark-anime-witcher.jpg"
+              className="card-img"
+            />
+            <Card.Title className="fs-2 mt-3 mb-2">Title</Card.Title>
+            <Card.Text className="mx-3 mb-3 text-light">
+              8 milhões de testes aplicados todos os anos em mais de 14.000
+              organizações de 160 países. O teste mais reconhecido do mundo para
+              avaliação de proficiência em inglês.
+            </Card.Text>
+          </Card>
+        </div>
+      </Col>
+    </>
+  );
+}
+
+export default MainCard;
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import MainCard from "../card";
 
 const Main: React.FC = () => {
   return (
-    <main>
-      <h1>Welcome to the Main Component</h1>
-      <p>This is the main content area of your application.</p>
+    <main className="text-center">
+      <h2 className="my-5 fs-1">All Stories</h2>
+      <section className="section-stories d-flex flex-wrap justify-content-center">
+        <Container>
+          <Row className="mb-5 row-gap-5">
+            <MainCard />
+            <MainCard />
+            <MainCard />
+            <MainCard />
+            <MainCard />
+            <MainCard />
+          </Row>
+        </Container>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
