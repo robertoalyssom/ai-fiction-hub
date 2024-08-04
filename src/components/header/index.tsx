1c3dd5a7a495937beed514aa921647e9bfc7c3c8
robertoalyssom <robert_alys@hotmail.com>
2024-08-04 20:31:31 -0300

    Cleaned up code: removed comments and fixed indentation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,24 @@
-import './header.css'
+import "./styles.css";
 
 function Header() {
   return (
-    <header className='header'>
-          <h1>
-            <a href="#"><img src="../../../assets/images/logo.png" alt="" className="header-logo" /></a>
-          </h1>
-        <div>
-          <h2 className='header-title fs-2'>AI Fiction hub</h2>
-          <p>Tales from Artificial Intelligence</p>
-        </div>
+    <header className="header">
+      <h1>
+        <a href="#">
+          <img
+            // test image from 'src' folder, to stop error messages
+            src="../../../assets/images/logo.png"
+            alt=""
+            className="header-logo"
+          />
+        </a>
+      </h1>
+      <div>
+        <h2 className="header-title fs-2">AI Fiction hub</h2>
+        <p>Tales from Artificial Intelligence</p>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header 
+export default Header;
