2b68219edd24d6fb89f596f60f9e1ea89375f5c2
robertoalyssom <robert_alys@hotmail.com>
2024-08-08 15:22:41 -0300

    style cards

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -8,11 +8,14 @@ function MainCard() {
       <Col sm={6} lg={4} className="alig-items-center justify-content-center">
         <div className="card-wrapper">
           <Card className="text-center card-main">
-            <Card.Img
-              variant="top"
-              src="../../../assets/images/dark-anime-witcher.jpg"
-              className="card-img"
-            />
+            <div className="card-img_ctn">
+              <Card.Img
+                variant="top"
+                src="../../../assets/images/dark-anime-witcher.jpg"
+                className="card-img"
+              />
+            </div>
+
             <Card.Title className="fs-2 mt-3 mb-2">Title</Card.Title>
             <Card.Text className="mx-3 mb-3 text-light">
               8 milhões de testes aplicados todos os anos em mais de 14.000
